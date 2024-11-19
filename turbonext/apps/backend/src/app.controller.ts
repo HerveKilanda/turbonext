a9d5fc72a2bc722e92a46e5d0723ff0028bc9f4a
Hervekilanda <kilandaherve@gmail.com>
2024-11-19 13:34:07 +0100

    initialisation de turborepo

--- turbonext/apps/backend/src/app.controller.ts
+++ turbonext/apps/backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from '@repo/types';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,9 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
   }
 }
